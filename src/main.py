06ded6fd8e74abb34a8c8fc01f5cfe7acf3ead71
hybridgreen <y.yayaoye@gmail.com>
2025-06-03 13:12:19 +0900

    Added static content to static folder. Wrote copy_to_public function in main()

--- src/main.py
+++ src/main.py
@@ -1,10 +1,46 @@
 from textnode import *
 from leafnode import * 
+import os , re, blocks, utility
+from os import path
+from shutil import copy, rmtree
+
+def copy_to_public(source, dest):
+    source_cont = os.listdir(source)
+    for item in source_cont:
+        item_path = path.join(source,item)
+        if path.isfile(item_path):
+            copy(item_path, dest)
+        else:
+            d_dir_path = path.join(dest,item)
+            os.mkdir(d_dir_path)
+            copy_to_public(item_path,d_dir_path)
+    pass
+
+def extract_title(markdown):
+    md_blocks = blocks.markdown_to_blocks(markdown) #to be deleted
+    for block in md_blocks:
+        if re.search(r"^#/s", block):
+            return block[:1].strip()
+        else:
+            raise Exception("Invalid markdown- Page must have a #Title")
 
 def main():
-    node = TextNode("Lorem Ipsum", TextType.LINK, "www.lipsum.com/")
+    #Set working directory and establish project structure
+    project_dir = os.getcwd()
+    static_dir = path.join(project_dir,"static")
+    public_dir = path.join(project_dir,"public")
 
-    print(node)
+    # Create a clean public directory
+    rmtree(public_dir)
+    os.mkdir(public_dir)
+
+    #Copy static content to public directory
+    if path.exists(static_dir):
+        copy_to_public(static_dir, public_dir)
+    else: 
+        print("Path error")
+        raise Exception("Path error")
     pass
 
 main()
+
