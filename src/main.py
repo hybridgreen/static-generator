391a0dd6b49fd75534a4ded79599bc64919d53ec
hybridgreen <y.yayaoye@gmail.com>
2025-06-04 14:01:35 +0900

    Reconfigured project to build for GitHub pages

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from leafnode import *
 import os , re, blocks, utility
 from os import path
 from shutil import copy, rmtree
+import sys
+
 
 def copy_to_public(source, dest):
     source_cont = os.listdir(source)
@@ -25,7 +27,7 @@ def extract_title(markdown):
         if re.search(r"^(#\s)", block):
             return block[1:].strip()
             
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from:\n -{from_path}\n -to {dest_path}\n -using {template_path}")
 
     with open(from_path) as md_file:
@@ -44,6 +46,8 @@ def generate_page(from_path, template_path, dest_path):
         content = blocks.markdown_to_html_node(markdown).to_html()
         template = template.replace("{{ Title }}", title)
         template = template.replace("{{ Content }}", content)
+        template = template.replace("href=\"/",f"href=\"{basepath}")
+        template = template.replace("src=\"/",f"src=\"{basepath}")
         print ("Markdown extraction: Success!")
     except Exception as e:
         print("Markdown extraction: Failed")
@@ -57,7 +61,7 @@ def generate_page(from_path, template_path, dest_path):
     index.close()
     pass
 
-def generate_pages_recursively(content_dir, template_path, dest_dir):
+def generate_pages_recursively(content_dir, template_path, dest_dir, basepath):
     
     content = os.listdir(content_dir)
 
@@ -65,7 +69,7 @@ def generate_pages_recursively(content_dir, template_path, dest_dir):
         item_path = path.join(content_dir, item)
         print("main.py/generate_recursive: " + item_path)
         if path.isfile(item_path):
-            generate_page(item_path, template_path, dest_dir)
+            generate_page(item_path, template_path, dest_dir, basepath)
         else:
             dest_sub_dir = path.join(dest_dir, item)
             try:
@@ -73,33 +77,42 @@ def generate_pages_recursively(content_dir, template_path, dest_dir):
             except:
                 print("Directory already exists. Continuing")
             
-            generate_pages_recursively(item_path, template_path, dest_sub_dir)
+            generate_pages_recursively(item_path, template_path, dest_sub_dir, basepath)
     pass
 
 def main():
+     
+    if len(sys.argv)>1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
     #Set working directory and establish project structure
     project_dir = os.getcwd()
     static_dir = path.join(project_dir,"static")
-    public_dir = path.join(project_dir,"public")
+    build_dir = path.join(project_dir,"docs")
     index_md = path.join(project_dir,"content/index.md")
     content_dir_path = path.join(project_dir, "content")
     template_path = path.join(project_dir,"template.html")
 
     # Create a clean public directory
-    rmtree(public_dir)
-    os.mkdir(public_dir)
+    try :
+        os.mkdir(build_dir)
+    except:
+        rmtree(build_dir)
+        os.mkdir(build_dir)
 
     #Copy static content to public directory
     if path.exists(static_dir):
-        copy_to_public(static_dir, public_dir)
+        copy_to_public(static_dir, build_dir)
     else: 
         print("Path error")
         raise Exception("Path error")
     
 
-    #generate_page(index_md, template_path,public_dir)
+    #generate_page(index_md, template_path,build_dir)
     #print("main.py: " + content_dir_path)
-    generate_pages_recursively(content_dir_path, template_path, public_dir)
+    generate_pages_recursively(content_dir_path, template_path, build_dir, basepath)
     pass
 
 main()
